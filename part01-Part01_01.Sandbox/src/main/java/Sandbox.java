af6bea5935826ec30eb0cef590da540edcbf0406
Ange-TOSSOU <angetossou.dev@gmail.com>
2024-03-27 07:49:56 +0100

    Initial commit

---
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Write your program here
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Your input: ");
+        String input = sc.nextLine();
+        
+        System.out.println("Your input is \'" + input + "\'");
+    }
+}
