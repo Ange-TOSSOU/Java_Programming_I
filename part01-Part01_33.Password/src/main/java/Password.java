09536bccf62b0bc9129068a54855078e691027ca
Ange-TOSSOU <angetossou.dev@gmail.com>
2024-03-30 18:06:53 +0100

    Sat, 30 Mar 2024 18:06:53 +0100 Update

--- part01-Part01_33.Password/src/main/java/Password.java
+++ part01-Part01_33.Password/src/main/java/Password.java
@@ -7,5 +7,13 @@ public class Password {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here 
+        System.out.println("Password?");
+        String pwd = scan.nextLine();
+        
+        if (pwd.equals("Caput Draconis")) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Off with you!");
+        }
     }
 }
