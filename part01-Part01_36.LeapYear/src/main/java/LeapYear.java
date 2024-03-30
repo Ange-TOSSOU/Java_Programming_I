a8cd0ed2e961159bc1a06107076aae7c7bddd174
Ange-TOSSOU <angetossou.dev@gmail.com>
2024-03-30 22:01:58 +0100

    Sat, 30 Mar 2024 22:01:58 +0100 Update

--- part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -5,6 +5,25 @@ public class LeapYear {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        
+        System.out.print("Give a year: ");
+        int year = Integer.valueOf(scan.nextLine());
+        
+        boolean isLeapYear = false;
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                isLeapYear = true;
+            }
+        } else {
+            if (year % 4 == 0) {
+                isLeapYear = true;
+            }
+        }
+        
+        if (isLeapYear) {
+            System.out.println("The year is a leap year.");
+        } else {
+            System.out.println("The year is not a leap year.");
+        }
     }
 }
