ddcbf183813a44de670521cde6530a093c7d2e93
Ange-TOSSOU <angetossou.dev@gmail.com>
2024-03-31 18:18:30 +0200

    Sun, 31 Mar 2024 18:18:30 +0200 Update

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -4,8 +4,18 @@ import java.util.Scanner;
 public class Sandbox {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         // Write your program here
         
-        System.out.print(2147483647 * 3);
+        while (true) {
+            System.out.println("Shall we carry on?");
+            String input = scanner.nextLine();
+            
+            if (input.equals("no")) {
+                break;
+            }
+        }
+        
+        System.out.println("LOL");
     }
 }
--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,14 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int first = Integer.valueOf(scanner.nextLine());
+        int second = Integer.valueOf(scanner.nextLine());
+        
+        int sum = first + second;
+        
+        if (sum >= 0) {
+            System.out.println(Math.sqrt(sum));
+        }
     }
 }
