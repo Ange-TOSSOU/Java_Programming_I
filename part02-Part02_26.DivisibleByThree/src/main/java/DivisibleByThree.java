d89e1ec9419ef2cb5dfefbc1b4d55bc2a4ed3af3
Ange-TOSSOU <angetossou.dev@gmail.com>
2024-04-04 16:55:43 +0200

    Thu, 04 Apr 2024 16:55:43 +0200 Update

--- part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -7,5 +7,17 @@ public class DivisibleByThree {
         Scanner scanner = new Scanner(System.in);
 
     }
+    
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        int a = beginning / 3;
+        if (beginning % 3 != 0) {
+            a++;
+        }
+        int b = end / 3;
+        
+        for (int i = a; i <= b; i++) {
+            System.out.println(3 * i);
+        }
+    }
 
 }
